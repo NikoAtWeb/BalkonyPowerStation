4ed5857a0be7cc487312c7de82e1386fc88dabd8
NikoAtWeb <niko.at.web@googlemail.com>
2022-11-26 23:50:41 +0100

    Added DPM request; better structure

--- lib/dpm86CTRL/src/dpm86CTRL.cpp
+++ lib/dpm86CTRL/src/dpm86CTRL.cpp
@@ -9,7 +9,7 @@ dpm86CTRL::dpm86CTRL()
 
 }
   //
-  // +++++++++++++++++++++++++++++++++++++++++++++++++
+  // ====================================================================================
   //
 void dpm86CTRL::begin()
 {
@@ -21,7 +21,7 @@ void dpm86CTRL::begin()
   #endif
 }
   //
-  // +++++++++++++++++++++++++++++++++++++++++++++++++
+  // ====================================================================================
   //
 void dpm86CTRL::setup(int _EnPin, int _RxPin, int _TxPin)
 { // well, needs to be checked how to define SoftwareSerial Pins in Lib
@@ -32,38 +32,61 @@ void dpm86CTRL::setup(int _EnPin, int _RxPin, int _TxPin)
   #endif
 }
   //
-  // +++++++++++++++++++++++++++++++++++++++++++++++++
+  // ====================================================================================
   //
-void dpm86CTRL::setup(int _EnPin)
+void dpm86CTRL::setup(int EnPin)
 {
+  _EnPin = EnPin; // swap to internal variable
   pinMode(_EnPin, OUTPUT);
   digitalWrite(_EnPin, LOW);
 }
   //
-  // +++++++++++++++++++++++++++++++++++++++++++++++++
+  // ====================================================================================
   //
-void dpm86CTRL::loop()
+void dpm86CTRL::SetResponse()
 {
   // read incoming message and print it on debug port
-    if(Serial.available( ) > 0) //  It will only send data when the received data is greater than 0.  
+    while(dpmSerialSoft.available( ) > 0) //  It will only send data when the received data is greater than 0.  
   {  
-    int arrivingdatabyte = dpmSerialSoft.read( );  // It will read the incoming or arriving data byte  
-    debugSerialPrint("data byte received: ");  
-    debugSerialPrintln(arrivingdatabyte);  
-  } 
-}
-  //
-  // +++++++++++++++++++++++++++++++++++++++++++++++++
-  //
-void dpm86CTRL::setVoltage(int _voltage)
-{
-  //sendOUT(_voltage);
-  sendOUT();
-}
+    // ++++++++++ READ incoming Message +++++++++++++++
 
+    _incomeStr = dpmSerialSoft.readString();
+
+    //++++++++++ debug incoming Message +++++++++++++++
+    
+    debugSerialPrint("Receive: ");
+    debugSerialPrintln(_incomeStr);
+
+  } //end while
+} // end loop
+  //
+  // ====================================================================================
+  //
+void dpm86CTRL::setVoltage(int voltage)
+{ 
+  _voltage = voltage;
+  // ++++++++++++++++++++++++++++++++++++++++++++++++++++
+  sendOUT();
+  SetResponse();
+  // ++++++++++++++++++++++++++++++++++++++++++++++++++++
+}
+  //
+  // ====================================================================================
+  //
+void dpm86CTRL::setCurrent(int current)
+{ 
+  _current = current;
+  // ++++++++++++++++++++++++++++++++++++++++++++++++++++
+  //sendOUT();
+  //SetResponse();
+  // ++++++++++++++++++++++++++++++++++++++++++++++++++++
+}
+  //
+  // ====================================================================================
+  //
 void dpm86CTRL::sendOUT()//(int _voltage)
 { // command address write volt = 12 fastresponse end
-// Example: :01w10=1200.\n
+  // Example: :01w10=1200.\n
 
   _cmd  = ":";
   _addr = 01;
@@ -83,8 +106,10 @@ void dpm86CTRL::sendOUT()//(int _voltage)
   // Send to MAX485
 
 #ifdef HARDWARE_SERIAL_ENABLE
+    dpmSerialHard.flush;
     dpmSerialHard.print(_sendOut);
 #else
+    dpmSerialSoft.flush();
     dpmSerialSoft.print(_sendOut);
 #endif
 
--- lib/dpm86CTRL/src/dpm86CTRL.h
+++ lib/dpm86CTRL/src/dpm86CTRL.h
@@ -12,20 +12,20 @@ class dpm86CTRL {
       //dpm86CTRL(HardwareSerial * SerialConnection); // ;
       dpm86CTRL();
       void begin();                                   // Begin Serial Connection
-      void setup(int _EnPin, int _RxPin, int _TxPin); // with SoftSerial PIN definition
-      void setup(int _EnPin);                         // only enable pin
-      void loop();                                    // read from Bus
-      void setVoltage(int _voltage);                  // set voltage of DPM86xx
+      void setup(int EnPin, int RxPin, int TxPin); // with SoftSerial PIN definition
+      void setup(int EnPin);                         // only enable pin
+      void SetResponse();                                    // read from Bus
+      void setVoltage(int voltage);
+      void setCurrent(int current);                // set voltage of DPM86xx
       void sendOUT();//(int _voltage);                  // send command to DPM86xx
-
-      int _EnPin;
-      int _voltage;
 
   private:
     int _addr;
     int _RxPin;
     int _TxPin;
-    String  _cmd, _v, _sendOut;
+    int _EnPin;
+    int _voltage, _current;
+    String  _cmd, _v, _sendOut;          String _incomeStr;
 };
 
 #endif
